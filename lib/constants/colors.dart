901ca20b38e2f4ae6a79fa0f4735465c63b72261
Md Irshad <md@Mds-MacBook-Air.local>
2023-11-07 10:08:42 +0530

    its a notes

---
+++ lib/constants/colors.dart
@@ -0,0 +1,14 @@
+  import 'package:flutter/material.dart';
+
+List<Color> backgroundColors = [
+    const Color(0xFFFFFFFF), // light blue
+    const Color(0xFFFFFFFF), // pale green
+    const Color(0xFFFFFFFF), // pale yellow
+    const Color(0xFFFFFFFF), // beige
+    const Color(0xFFFFFFFF), // light pink
+    const Color(0xFFFFFFFF), // light grey
+    const Color(0xFFFFFFFF), // pale pink
+    const Color(0xFFFFFFFF), // pale blue
+    const Color(0xFFFFFFFF), // mint green
+    const Color(0xFFFFFFFF), // pale orange
+  ];
